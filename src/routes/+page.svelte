81225c8bfce12711ba76389b3b88f0d5f5e802b7
Siddeeq Rasheed <srasheed1018@bths.edu>
2023-11-28 00:22:31 -0500

    installed flowbite

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,7 +5,11 @@
 		<h2 class="h2 bg-gradient-to-br from-blue-500 to-cyan-300 bg-clip-text text-transparent box-decoration-clone">
 			Siddeeq Rasheed
 		</h2>
+		
 	</div>
+	
+	
+
 </div>
 
 <style lang="postcss">
