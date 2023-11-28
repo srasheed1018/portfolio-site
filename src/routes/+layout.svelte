11ecb3bd3d25c5928355d481674bde15b17b1c00
Siddeeq Rasheed <srasheed1018@gmail.com>
2023-11-28 02:36:12 -0500

    scrapped AppBar component

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,6 @@
 <script lang="ts">
 	import '../app.postcss';
 	import { AppShell, AppBar } from '@skeletonlabs/skeleton';
-
-	// Floating UI for Popups
-	import { computePosition, autoUpdate, flip, shift, offset, arrow } from '@floating-ui/dom';
-	import { storePopup } from '@skeletonlabs/skeleton';
-	storePopup.set({ computePosition, autoUpdate, flip, shift, offset, arrow });
 </script>
 
 <!-- App Shell -->
@@ -28,17 +23,7 @@
 	<!-- (header) -->
 	<!-- (sidebarLeft) -->
 	<!-- (sidebarRight) -->
-	<svelte:fragment slot="pageHeader">
-		<AppBar>
-			<svelte:fragment slot="lead">
-				<img src="favicon.png" alt="logo"/>
-			</svelte:fragment>
-				siddeeq.tech
-			<svelte:fragment slot="trail">
-				buttons go here
-			</svelte:fragment>
-		</AppBar>
-	</svelte:fragment>
+	<!-- (pageHeader) -->
 	<!-- Router Slot -->
 	<slot />
 	<!-- ---- / ---- -->
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,17 @@
-<!-- YOU CAN DELETE EVERYTHING IN THIS PAGE -->
+<script>
+	
+</script>
 
 <div class="container h-full mx-auto flex justify-center items-center">
 	<div class="space-y-10 text-center flex flex-col items-center">
-		<h2 class="h2 bg-gradient-to-br from-blue-500 to-cyan-300 bg-clip-text text-transparent box-decoration-clone">
-			Siddeeq Rasheed
-		</h2>
+		<div class="bg-gradient-to-br from-blue-500 to-cyan-300 bg-clip-text text-transparent box-decoration-clone">
+			<h2 class="h2">
+				My name is <b>Siddeeq Rasheed</b>,
+			</h2>
+			<h3 class="h3">
+				and I'm an aspiring software engineer.
+			</h3>
+		</div>
 		
 	</div>
 	
